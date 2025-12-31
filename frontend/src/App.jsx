1e421eafd3c02c869277347df3ceefdcb4a5eac6
Chandrashekhar Sharma <sharmachiman6@gmail.com>
2025-12-31 16:32:17 +0530

    frontend setup and SignUp&LogIn pages setup.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import SignUp from './pages/SignUp'
+import LogIn from './pages/LogIn'
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/signup" element={<SignUp/>} />
+      <Route path="/login" element={<LogIn/>} />
+    </Routes>
+  )
+}
+
+export default App
