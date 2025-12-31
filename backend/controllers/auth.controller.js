35f8a8676e31613a37534ff7d077e58d83f68f49
Chandrashekhar Sharma <sharmachiman6@gmail.com>
2025-12-31 11:15:12 +0530

    Log-Out Api done!

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -93,3 +93,12 @@ export const logIn = async (req, res) => {
     return res.status(500).json(error);
   }
 };
+
+export const logOut=async (req,res)=>{
+  try {
+    res.clearCookie("token")
+    return res.status(200).json({message:"LogOut Successfully!"})
+  } catch (error) {
+   return res.status(400).json(error) 
+  }
+}
